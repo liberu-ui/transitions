647048c08bf977a4af8dde536c036a7d97fbdfa3
mishu <mishu@xtelecom.ro>
2019-02-18 18:51:13 +0200

    adds mode prop

--- src/transitions/Fade.vue
+++ src/transitions/Fade.vue
@@ -1,8 +1,19 @@
 <template>
     <transition appear
+        :mode="mode"
         enter-active-class="fadeIn"
         leave-active-class="fadeOut"
         v-on="$listeners">
         <slot/>
     </transition>
 </template>
+
+<script>
+export default {
+    props: {
+        mode: {
+            default: null,
+        },
+    },
+}
+</script>
