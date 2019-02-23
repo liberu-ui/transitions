e02de5e4d53ff03a14f6a882bc48b68d7f236f79
mishu <mishu@xtelecom.ro>
2019-02-23 11:23:15 +0200

    fixes slide up/down  transitions

--- src/transitions/SlideDown.vue
+++ src/transitions/SlideDown.vue
@@ -1,7 +1,7 @@
 <template>
     <transition appear
         enter-active-class="animated slideInDown"
-        leave-active-class="animated slideOutDown"
+        leave-active-class="animated slideOutUp"
         v-bind="$attrs"
         v-on="$listeners">
         <slot/>
--- src/transitions/SlideUp.vue
+++ src/transitions/SlideUp.vue
@@ -1,7 +1,7 @@
 <template>
     <transition appear
         enter-active-class="animated slideInUp"
-        leave-active-class="animated slideOutUp"
+        leave-active-class="animated slideOutDown"
         v-bind="$attrs"
         v-on="$listeners">
         <slot/>
