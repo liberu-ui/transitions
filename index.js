a2ddbe3140800a2a1a71508927efa57ed5f5ee29
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-07 20:36:08 +0000

    Replace JavaScript with TypeScript

--- index.js
+++
@@ -1,5 +0,0 @@
-import Fade from './src/transitions/Fade.vue';
-import Zoom from './src/transitions/Zoom.vue';
-import Slide from './src/transitions/Slide.vue';
-
-export { Fade, Zoom, Slide };
---
+++ index.ts
@@ -0,0 +1,22 @@
+/**
+ * This module exports three transition components: Fade, Zoom, and Slide.
+ */
+
+import Fade from './src/transitions/Fade.vue';
+import Zoom from './src/transitions/Zoom.vue';
+import Slide from './src/transitions/Slide.vue';
+
+/**
+ * The Fade component provides a transition effect that fades in or out an element.
+ */
+export { Fade };
+
+/**
+ * The Zoom component provides a transition effect that zooms in or out an element.
+ */
+export { Zoom };
+
+/**
+ * The Slide component provides a transition effect that slides an element in or out from a specific direction.
+ */
+export { Slide };
